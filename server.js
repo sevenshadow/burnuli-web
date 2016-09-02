183e27209adfc8a9b8570cb7cabd1d0880424a6a
sevenshadow <charlie@sevenshadow.com>
2016-09-02 15:34:11 -0400

    Add new express server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 1337;
+
+app.use(express.static(__dirname + '/dist'));
+app.listen(port);
