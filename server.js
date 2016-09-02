2763a70f50c6bb0f6fab5edd39e499c31de74087
sevenshadow <charlie@sevenshadow.com>
2016-09-02 16:03:32 -0400

    Handle wildcard routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express'),
+path = require('path'),
 app = express(),
 port = process.env.PORT || 1337;
 
 app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '/dist/index.html'));
+});
 app.listen(port);
