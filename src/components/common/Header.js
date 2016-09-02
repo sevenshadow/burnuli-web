477d39f0c9eff0445ab2937866c5fef3a8266b35
sevenshadow <charlie@sevenshadow.com>
2016-09-02 17:29:23 -0400

    Remove top navigation.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,7 +4,10 @@ import {Link, IndexLink} from 'react-router';
 const Header = () =>  {
     return (
       <nav className="navbar">
-              <IndexLink to="/" activeClassName="topNavActive">Home</IndexLink>
+      </nav>
+    );
+    /*
+    <IndexLink to="/" activeClassName="topNavActive">Home</IndexLink>
               <span> | </span>
               <Link to="/services" activeClassName="topNavActive">Services</Link>
               <span> | </span>
@@ -13,10 +16,7 @@ const Header = () =>  {
               <Link to="/about" activeClassName="topNavActive">About</Link>
               <span> | </span>
               <Link to="/contact" activeClassName="topNavActive">Contact</Link>
-               
-      </nav>
-    );
-  
+    */          
 };
 
 export default Header;
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,11 +3,12 @@ import {Link} from 'react-router';
 
 class HomePage extends React.Component {
   render() {
+    //        <!-- <Link to="/about" className="btn btn-primary btn-lg">Learn More</Link> -->
     return (
       <div className="jumbotron">
         <h1>Seven Shadow LLC</h1>
-        <p>Application Development Services</p>
-        <Link to="/about" className="btn btn-primary btn-lg">Learn More</Link>
+        <p>Financial Analysis and Application Development Services</p>
+        <p><a className="btn btn-primary btn-lg" href="mailto:info@sevenshadow.com">More Information</a></p>
       </div>
     );
   }
