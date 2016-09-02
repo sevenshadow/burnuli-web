dc10f0eadeef0d50a48f1c98781e38ff58576031
sevenshadow <charlie@sevenshadow.com>
2016-09-02 16:11:26 -0400

    Test the paths

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ path = require('path'),
 app = express(),
 port = process.env.PORT || 1337;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join( __dirname, '/dist/index.html'));
+  res.sendFile(path.join( __dirname, '/index.html'));
 });
 app.listen(port);
