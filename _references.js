be1ab11b133e61dabcc5bdb54d600b748ca073ba
sevenshadow <charlie@sevenshadow.com>
2016-09-09 09:00:33 -0400

    Remove routing

--- _references.js
+++
@@ -1,3 +0,0 @@
-/// <reference path="typings/node/node.d.ts" />
-/// <reference path="typings/express/express.d.ts" />
-/// <reference path="typings/jquery/jquery.d.ts" />
--- server.js
+++ server.js
@@ -1,34 +1,26 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes/index')
-  , user = require('./routes/user')
   , http = require('http')
+  , favicon = require('express-favicon')
   , path = require('path');
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(favicon(__dirname + '/public/favicon.png'));
+app.use(express.static(path.join(__dirname, 'public')));
+  
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+//app.configure('development', function(){
+//  app.use(express.errorHandler());
+//});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join( __dirname, '/public/index.html'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.get('/projects', function(req, res) {
+  res.sendFile(path.join( __dirname, '/public/index.html'));
 });
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
