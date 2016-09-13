5df21d24323798bb88660eff3953a896ada67e51
sevenshadow <charlie@sevenshadow.com>
2016-09-13 09:10:12 -0400

    Remove about route

--- server.js
+++ server.js
@@ -26,11 +26,6 @@ app.get('/services', function(req, res) {
   res.sendFile(path.join( __dirname, '/public/index.html'));
 });
 
-app.get('/about', function(req, res) {
-  res.sendFile(path.join( __dirname, '/public/index.html'));
-});
-
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
