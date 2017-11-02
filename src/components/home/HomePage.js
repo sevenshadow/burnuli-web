74a9983fca69057620e310c418c26df95e868017
Charlie Strout <charlie@sevenshadow.com>
2017-11-02 19:04:40 -0400

    changne page title

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -11,17 +11,8 @@ class HomePage extends React.Component {
       <div className="main-wrapper">
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 main-content">
-            <h1>Seven Shadow LLC</h1>
+            <h1>Burnuli</h1>
             <p>Financial Analysis and Application Development</p>
-            <div>
-              <Link to="http://morningmonte.com" className="btn btn-primary" target="_self">Morning Monte</Link> 
-            </div>
-            <div>
-              <Link to="https://prudena.com" className="btn btn-warning" target="_self">Prudena</Link>
-            </div>
-            <div>
-              <Link to="https://twitter.com/sevenshadow" className="btn btn-success" target="_self">Twitter</Link>
-            </div>
             <div className="add-margin-top-20">
               e. <Link to="mailto:info@sevenshadow.com" target="_self">info@sevenshadow.com</Link> t. <Link to="tel:646-355-8806" target="_self">+1.646.355.8806</Link>
             </div>
